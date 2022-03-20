1355d17a586a740c50f8f201a417fb15bb01e1e6
George991808 <gellis7@gmail.com>
2022-03-20 20:01:16 +0800

    adding to the models file

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Blog extends Model {}
+class Comment extends Model {}
 
-Blog.init(
+Comment.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -15,7 +15,7 @@ Blog.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    post: {
+    comment: {
       type: DataTypes.STRING,
     },
     
@@ -31,6 +31,14 @@ Blog.init(
         model: 'user',
         key: 'id',
       },
+     
+    },
+        blog_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'blog',
+          key: 'id',
+    },
     },
   },
   {
@@ -38,8 +46,8 @@ Blog.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'blog',
+    modelName: 'comment',
   }
 );
 
-module.exports = Blog;
+module.exports = Comment;
