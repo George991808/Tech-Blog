38596e7a8f0d296d1e49043436dd9e459e9c8e43
George991808 <gellis7@gmail.com>
2022-03-21 18:46:14 +0800

    made a lot of changes to routes and handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,44 +2,54 @@ const router = require('express').Router();
 const Blog = require('../models/Blog');
 const User = require('../models/User');
 
-// route to get all Bloges
+// GET all galleries for homepage
 router.get('/', async (req, res) => {
-  const BlogData = await Blog.findAll().catch((err) => {
-    res.json(err);
-  });
-  const Blogs = BlogData.map((Blog) => Blog.get({ plain: true }));
-  res.render('home', { Blogs });
-});
-
-// route to get all Bloges
-router.get('/login/', async (req, res) => {
-  const BlogData = await Blog.findAll().catch((err) => {
-    res.json(err);
-  });
-  const Blogs = BlogData.map((Blog) => Blog.get({ plain: true }));
-  res.render('login', { Blogs });
-});
-// router.get('/login', async (req, res) => {
-//   // find all categories
-//   // be sure to include its associated Products
-//   const bookData = await User.findAll();
-
-//   return res.json(bookData);
-// });
-
-// route to get one Blog
-router.get('/Blog/:id', async (req, res) => {
+  // if (!req.session.loggedIn) {
+  //   res.redirect('/login');
+  // } else {
   try {
-    const BlogData = await Blog.findByPk(req.params.id);
-    if (!BlogData) {
-      res.status(404).json({ message: 'No Blog with this id!' });
-      return;
-    }
-    const Blog = BlogData.get({ plain: true });
-    res.render('Blog', Blog);
+    const dbBlogData = await Blog.findAll();
+
+    const blog = dbBlogData.map((blog) => blog.get({ plain: true }));
+
+    res.render('home', {
+      loggedIn: req.session.loggedIn,
+      blog,
+    });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
+  // }
+});
+
+// GET one gallery
+router.get('/blog/:id', async (req, res) => {
+  // If the user is not logged in, redirect the user to the login page
+  // if (!req.session.loggedIn) {
+  //   res.redirect('/login');
+  // } else {
+  // If the user is logged in, allow them to view the gallery
+  try {
+    const dbBlogData = await Blog.findByPk(req.params.id, {});
+    const blog = dbBlogData.get({ plain: true });
+    const dbUserData = await User.findByPk(blog.user_id, {});
+    const user = dbUserData.get({ plain: true });
+    res.render('blog', { blog, user, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+  // }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
